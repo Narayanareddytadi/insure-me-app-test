ff9d4a78c04de832fdf8c2153a9f7cf4655e9ba1
Narayanareddytadi <100251230+Narayanareddytadi@users.noreply.github.com>
2024-08-18 14:28:39 -0400

    Update App.java by adding new app ip address

--- src/main/java/com/testing/insure_me_test_app/App.java
+++ src/main/java/com/testing/insure_me_test_app/App.java
@@ -22,7 +22,7 @@ public class App
 	 	WebDriver driver = new ChromeDriver(options);
 	 	
 	 	//open the web application
-        driver.get("http://18.222.0.17:8081/contact.html");
+        driver.get("http://54.242.160.54:8081/contact.html");
         
         
         String title = driver.getTitle();
